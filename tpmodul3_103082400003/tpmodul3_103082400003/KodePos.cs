de489e9d882f38f1fbb055dffe87a16aae82a56a
ChandraRasyid <chandrarasyid06@gmail.com>
2026-03-15 19:49:19 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul3_103082400003/tpmodul3_103082400003/KodePos.cs
+++ tpmodul3_103082400003/tpmodul3_103082400003/KodePos.cs
@@ -6,5 +6,22 @@ namespace tpmodul3_103082400003
 {
     internal class KodePos
     {
+        private string[,] listKodePos= {
+                { "Batununggal  ", "40266" },
+                { "Kujangsari   ", "40287" },
+                { "Mengger      ", "40267" },
+                { "Wates        ", "40256" },
+                { "Cijaura      ", "40287" },
+                { "Jatisari     ", "40286" },
+                { "Margasari    ", "40286" },
+                { "Sekejati     ", "40286" },
+                { "Kebonwaru    ", "40272" },
+                { "Maleer       ", "40274" },
+                { "Samoja       ", "40273" }
+        };
+        public string[,] getKodePos()
+        {
+            return listKodePos; 
+        }
     }
 }
--- tpmodul3_103082400003/tpmodul3_103082400003/Program.cs
+++ tpmodul3_103082400003/tpmodul3_103082400003/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul3_103082400003;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kp = new KodePos();
+        string[,] listkp = kp.getKodePos();
+
+        Console.WriteLine("Daftar Kelurahan dan Kode Pos");
+        Console.WriteLine("--------------------------------------------------");
+        for (int i = 0; i < listkp.GetLength(0); i++)
+        {
+            Console.WriteLine("Kelurahan : " + listkp[i,0] + "| Kode Pos : " + listkp[i, 1]);
+        }
+    }
+}
